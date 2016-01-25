7a149be7df1315a309cdfb4aed942c4a163ded4a
Laufey Rut Guðmundsdóttir <Laufey@Laufeys-MacBook-Pro.local>
2016-01-25 14:54:11 +0000

    undo og redo reddy

--- js/App.js
+++ js/App.js
@@ -34,6 +34,9 @@ function App(canvasSelector) {
 			pos.log('drawing stop');
 
 			self.shapes.push(shape);
+
+			// Empty the redo array 
+			self.undoShapes = [];
 			shape.added(self.canvasContext);
 
 			// Remove drawing and drawingStop functions from the mouse events
@@ -46,7 +49,7 @@ function App(canvasSelector) {
 		};
 
 		// Add drawing and drawingStop functions to the mousemove and mouseup events
-		self.canvas.on({
+		self.canvas.on({	
 			mousemove:drawing,
 			mouseup:drawingStop
 		});
@@ -80,6 +83,105 @@ function App(canvasSelector) {
 		self.redraw();
 	};
 
+	self.undo = function() {
+		var undoShape = self.shapes.pop();
+		self.undoShapes.push(undoShape);
+		self.redraw();
+
+	};
+
+	self.save = function() {
+		var stringifiedArray = JSON.stringify(self.shapes);
+			var param = { "user": "laufey14", // You should use your own username!
+				"name": "test1", //title,
+				"content": stringifiedArray,
+				"template": true
+			};
+
+			$.ajax({
+				type: "POST",
+				contentType: "application/json; charset=utf-8",
+				url: "http://whiteboard.apphb.com/Home/Save",
+				data: param,
+				dataType: "jsonp",
+				crossDomain: true,
+				success: function (data) {
+					// The save was successful...
+					console.log("worked");
+				},
+				error: function (xhr, err) {
+					// Something went wrong...
+					console.log("nat");
+
+				}
+			});
+	};
+
+	self.loadDrawingList = function() {
+		var stringifiedArray = JSON.stringify(self.shapes);
+			var param = { "user": "laufey14", // You should use your own username!
+				"name": "test1",
+				"content": stringifiedArray,
+				"template": true
+			};
+
+			$.ajax({
+				type: "POST",
+				contentType: "application/json; charset=utf-8",
+				url: "http://whiteboard.apphb.com/Home/GetList",
+				data: param,
+				dataType: "jsonp",
+				crossDomain: true,
+				success: function (data) {
+					self.redraw(data);
+					console.log(data);
+				},
+				error: function (xhr, err) {
+					// Something went wrong...
+					console.log("nat");
+
+				}
+			});
+	};
+
+	self.loadDrawing = function() {
+		var stringifiedArray = JSON.stringify(self.shapes);
+			var param = { "user": "laufey14", // You should use your own username!
+				"id" : 3599 // ekki harðkóða
+			};
+
+			$.ajax({
+				type: "POST",
+				contentType: "application/json; charset=utf-8",
+				url: "http://whiteboard.apphb.com/Home/GetWhiteboard",
+				data: param,
+				dataType: "jsonp",
+				crossDomain: true,
+				success: function (data) {
+					self.redraw(data);
+					console.log(data);
+				},
+				error: function (xhr, err) {
+					// Something went wrong...
+					console.log("nat");
+
+				}
+			});
+	};
+
+	self.redo = function() {
+		if ( self.undoShapes.length > 0)
+		{
+			console.log("inní " + self.undoShapes);
+
+			var redoShape = self.undoShapes.pop();
+			self.shapes.push(redoShape);
+			self.redraw();
+		}
+		console.log(self.undoShapes);
+
+	};
+
 	self.setColor = function(color) {
 		self.color = color;
 	};
@@ -98,6 +200,8 @@ function App(canvasSelector) {
 		self.shapeFactory = null;
 		self.canvasContext = canvas.getContext("2d");
 		self.shapes = new Array();
+		self.undoShapes = new Array();
+
 
 		// Set defaults
 		self.color = '#ff0000';
@@ -129,11 +233,16 @@ $(function() {
 	  return new Textbox($('#font').val(), $('#fontSize').val(), $('#fontStyle').val());
 	};});
 	$('#clearbutton').click(function(){app.clear();});
+	$('#undobutton').click(function(){app.undo();});
+	$('#redobutton').click(function(){app.redo();});
+	$('#savebutton').click(function(){app.save();});
+	$('#loadDrawingbutton').click(function(){app.loadDrawing();});
+	$('#loadDrawingListbutton').click(function(){app.loadDrawingList();});
 	$('#color').change(function(){app.setColor($(this).val());});
 	$('#width').change(function(){app.setWidth($(this).val());});
 	$("control_id").attr("checked",true);
 	
-    var checked = document.getElementById("squarebutton");
+    var checked = document.getElementById("penbutton");
     checked.click();
 	
 });
